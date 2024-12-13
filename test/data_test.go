bd31bf69531a4098162b34d92539f9c8a3000609
a-korkin <a-korkin88@yandex.ru>
2024-12-13 14:33:17 +0300

    feat: getting note test

--- test/data_test.go
+++ test/data_test.go
@@ -8,36 +8,51 @@ import (
 	"testing"
 )
 
-func setup(dbConn *data.DbConnect, dbName *string) {
-	log.Printf("setup...")
-	dbConn.Db.Exec(fmt.Sprintf("create database %s;", *dbName))
+var DbConnect *data.DbConnect
+
+func connectToDb(dbName *string) {
+	conn := fmt.Sprintf(
+		"user=postgres password=admin dbname=%s sslmode=disable", *dbName)
+	DbConnect = data.NewDbConnect(conn)
 }
 
-func shutdown(dbConn *data.DbConnect, dbName *string) {
+func setup(dbName *string) {
+	log.Printf("setup...")
+	DbConnect.Db.Exec(fmt.Sprintf("create database %s;", *dbName))
+	connectToDb(dbName)
+	DbConnect.Db.Exec(`
+create table if not exists notes
+(
+	id serial primary key,
+	text varchar(255)
+);
+insert into notes(text)
+values('first note'), ('second note'), ('third note');
+`)
+}
+
+func shutdown() {
 	log.Printf("shutdown...")
-	dbConn.Db.Exec(fmt.Sprintf("drop database %s;", *dbName))
+	DbConnect.Db.Exec(fmt.Sprintf("drop table if exists notes;"))
 }
 
 func TestMain(m *testing.M) {
-	conn := "user=postgres password=admin sslmode=disable"
-	log.Printf("conn: %s", conn)
-	dbName := "notes_db_test"
-	dbConnect := data.NewDbConnect(conn)
-	setup(dbConnect, &dbName)
+	dbName := "postgres"
+	connectToDb(&dbName)
+
+	dbName = "notes_db_test"
+	setup(&dbName)
 	code := m.Run()
-	shutdown(dbConnect, &dbName)
+	shutdown()
 	os.Exit(code)
 }
 
-// func (dbConnect *DbConnect) GetNote(id int32) (*common.Note, error) {
-// 	row, err := dbConnect.Db.Query("select text from notes where id = $1", id)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if row.Next() {
-// 		note := common.Note{Id: id}
-// 		row.Scan(&note.Text)
-// 		return &note, nil
-// 	}
-// 	return nil, nil
-// }
+func TestGetNoteSucces(t *testing.T) {
+	note, err := DbConnect.GetNote(1)
+	if note.Text != "first note" {
+		t.Errorf("wrong note getted")
+	}
+	if err != nil {
+		t.Errorf("error of getting note: %s", err)
+	}
+}
